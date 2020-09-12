77a4347166e76716e4382e49634bd9267b3f267c
ch h <1648262054@qq.com>
2020-09-12 16:14:23 +0800

    Signed-off-by: ch h <1648262054@qq.com>

---
+++ dotnetzy1.3/dotnetzy1.3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace dotnetzy1._3
+{
+    class Program
+    {
+        static int readnum()
+        {
+            int num;
+            Console.WriteLine("请输入数据：");
+            num = int.Parse(Console.ReadLine());
+            return num;
+        }
+        static bool primenum(int num)
+        {
+            for(int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                    return false; 
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            int num;
+            num = readnum();
+            for(int i = 2; i < num; i++)
+            {
+                if((num%i==0)&&primenum(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
