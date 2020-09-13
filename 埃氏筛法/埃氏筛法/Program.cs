088a800d5d4cc29279fe8b6c81d68313032850bc
ch h <1648262054@qq.com>
2020-09-13 15:02:19 +0800

    Signed-off-by: ch h <1648262054@qq.com>

---
+++ 埃氏筛法/埃氏筛法/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace 埃氏筛法
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[101];
+            for (int i = 0; i <= 100; i++)
+            {
+                array[i] = i;
+            }
+            for (int i = 2; i <= 100; i++)
+            {
+                for (int j = 2; i * j <= 100; j++)
+                    array[i * j] = 0;
+            }
+            for (int i = 2; i <= 100; i++)
+            {
+                if (array[i] != 0)
+                    Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
---
+++ 整数数组求值/整数数组求值/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace 整数数组求值
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+            int max = array1[0], min = array1[0];
+            double avg = 0, sum = 0;
+            foreach (int i in array1)
+            {
+                if (i > max)
+                    max = i;
+                if (i < min)
+                    min = i;
+                sum += i;
+            }
+            avg = sum / array1.Length;
+            Console.WriteLine("最大值：" + max + " 最小值：" + min + " 平均值：" + avg + " 总和：" + sum);
+        }
+    }
+}
